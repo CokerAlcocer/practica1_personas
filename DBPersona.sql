eebdfb9526513dd8023ef5697fd3b575a7aee799
SergioCoP <84104056+SergioCoP@users.noreply.github.com>
2023-01-27 17:09:57 -0600

    Add files via upload

---
+++ DBPersona.sql
@@ -0,0 +1,20 @@
+
+Create database dbpersona;
+use dbpersona;
+
+create table persona (
+idPerson tinyint primary key auto_increment not null,
+nombre varchar(50),
+aPaterno varchar(50),
+aMaterno varchar(50),
+edad tinyint,
+sexo char,
+telefono varchar(15),
+direccion varchar(255),
+fechaNacimiento date,
+estadoCivil boolean,
+correo varchar(60),
+trabajo boolean,
+contrasena varchar(20),
+estado boolean
+)
