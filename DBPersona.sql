ca2e7e69fb853a489ec1def4c2ecb95087306c3b
CokerAlcocer <83984636+CokerAlcocer@users.noreply.github.com>
2023-02-01 14:55:44 -0600

    [UPDATE] Actualización del script de la base de datos y backend

--- DBPersona.sql
+++ DBPersona.sql
@@ -1,19 +1,73 @@
+CREATE DATABASE dbpersona;
+USE dbpersona;
 
-Create database dbpersona;
-use dbpersona;
-
-create table persona (
-idPersona tinyint primary key auto_increment not null,
-nombre varchar(50),
-aPaterno varchar(50),
-aMaterno varchar(50),
-edad tinyint,
-sexo varchar(20),
-telefono varchar(15),
-direccion varchar(255),
-fechaNacimiento date,
-estadoCivil varchar(20),
-correo varchar(60),
-contrasena varchar(20),
-estado boolean default true
+CREATE TABLE persona (
+    id BIGINT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    nombre VARCHAR(50) NOT NULL,
+    a_paterno VARCHAR(50) NOT NULL,
+    a_materno VARCHAR(50),
+    edad TINYINT NOT NULL,
+    sexo VARCHAR(20) NOT NULL,
+    telefono VARCHAR(15) UNIQUE KEY NOT NULL,
+    direccion VARCHAR(255) NOT NULL,
+    fecha_nacimiento DATE,
+    estado_civil VARCHAR(20) NOT NULL,
+    correo VARCHAR(100) UNIQUE KEY NOT NULL,
+    contrasena VARCHAR(20) NOT NULL,
+    estado boolean DEFAULT TRUE
 );
+
+CREATE TABLE tipo_operacion(
+    id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    nombre VARCHAR(20) UNIQUE KEY NOT NULL
+);
+
+CREATE TABLE json_persona(
+    id BIGINT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    datos JSON,
+    id_persona BIGINT NOT NULL, -- ID DE LA PERSONA A LA QUE PERTENECE ESE JSON
+    FOREIGN KEY (id_persona) REFERENCES persona(id)
+);
+
+CREATE TABLE operacion(
+    id BIGINT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    id_tipo_operacion INT NOT NULL,
+    dato_viejo JSON,
+    dato_nuevo JSON NOT NULL,
+    fecha DATETIME DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (id_tipo_operacion) REFERENCES tipo_operacion(id)
+);
+
+INSERT INTO tipo_operacion(nombre) VALUES ('INSERT');
+INSERT INTO tipo_operacion(nombre) VALUES ('UPDATE');
+INSERT INTO tipo_operacion(nombre) VALUES ('DELETE');
+INSERT INTO tipo_operacion(nombre) VALUES ('LOGIN');
+INSERT INTO tipo_operacion(nombre) VALUES ('LOGOUT');
+INSERT INTO tipo_operacion(nombre) VALUES ('FIND_ALL');
+INSERT INTO tipo_operacion(nombre) VALUES ('FIND_BY');
+
+DROP TRIGGER IF EXISTS operation_insert;
+DELIMITER //
+CREATE TRIGGER operation_insert AFTER INSERT ON json_persona FOR EACH ROW
+BEGIN
+    INSERT INTO operacion(id_tipo_operacion, dato_viejo, dato_nuevo) VALUES(1, '{}', new.datos);
+END //
+DELIMITER ;
+
+DROP TRIGGER IF EXISTS operation_update;
+DELIMITER //
+CREATE TRIGGER operation_update AFTER UPDATE ON json_persona FOR EACH ROW
+BEGIN
+    INSERT INTO operacion(id_tipo_operacion, dato_viejo, dato_nuevo) VALUES(2, old.datos, new.datos);
+END //
+DELIMITER ;
+
+DROP TRIGGER IF EXISTS operation_delete;
+DELIMITER //
+CREATE TRIGGER operation_delete AFTER DELETE ON json_persona FOR EACH ROW
+BEGIN
+    INSERT INTO operacion(id_tipo_operacion, dato_viejo, dato_nuevo) VALUES(3, old.datos, '{}');
+END //
+DELIMITER ;
+
+
--- src/main/java/utez/edu/mx/practica1/model/person/DaoPerson.java
+++ src/main/java/utez/edu/mx/practica1/model/person/DaoPerson.java
@@ -10,8 +10,6 @@ public class DaoPerson {
     //Variables globales
     Connection con;
     PreparedStatement pstm;
-    CallableStatement cstm;
-    Statement stm;
     ResultSet rs;
 
     // EncontrarTodos
@@ -24,23 +22,23 @@ public class DaoPerson {
             while (rs.next()) {
                 BeanPerson person = new BeanPerson();
 
-                person.setIdPerson(rs.getInt("idPersona"));
+                person.setId(rs.getLong("id"));
                 person.setNombre(rs.getString("nombre"));
-                person.setaPaterno(rs.getString("aPaterno"));
-                person.setaMaterno(rs.getString("aMaterno"));
+                person.setaPaterno(rs.getString("a_paterno"));
+                person.setaMaterno(rs.getString("a_materno"));
                 person.setEdad(rs.getInt("edad"));
                 person.setSexo(rs.getString("sexo"));
                 person.setTelefono(rs.getString("telefono"));
                 person.setDireccion(rs.getString("direccion"));
-                person.setFechaNacimiento(rs.getString("fechaNacimiento"));
-                person.setEstadoCivil(rs.getString("estadoCivil"));
+                person.setFechaNacimiento(rs.getString("fecha_nacimiento"));
+                person.setEstadoCivil(rs.getString("estado_civil"));
                 person.setCorreo(rs.getString("correo"));
                 person.setContrasena(rs.getString("contrasena"));
 
                 listPersons.add(person);
             }
         }catch (Exception e) {
-            e.getMessage();
+            System.out.println("DP_ERR_01");
         }finally {
             ConnectionDB.closeConnections(con,pstm);
         }
@@ -54,57 +52,64 @@ public class DaoPerson {
         BeanPerson person = null;
         try {
             con = ConnectionDB.getConnection();
-            pstm = con.prepareStatement("SELECT * FROM persona WHERE idPersona=?");
+            pstm = con.prepareStatement("SELECT * FROM persona WHERE idPersona = ?");
             pstm.setInt(1,id);
             rs = pstm.executeQuery();
 
             person = new BeanPerson();
 
-            person.setIdPerson(rs.getInt("idPersona"));
+            person.setId(rs.getLong("id"));
             person.setNombre(rs.getString("nombre"));
-            person.setaPaterno(rs.getString("aPaterno"));
-            person.setaMaterno(rs.getString("aMaterno"));
+            person.setaPaterno(rs.getString("a_paterno"));
+            person.setaMaterno(rs.getString("a_materno"));
             person.setEdad(rs.getInt("edad"));
             person.setSexo(rs.getString("sexo"));
             person.setTelefono(rs.getString("telefono"));
             person.setDireccion(rs.getString("direccion"));
-            person.setFechaNacimiento(rs.getString("fechaNacimiento"));
-            person.setEstadoCivil(rs.getString("estadoCivil"));
+            person.setFechaNacimiento(rs.getString("fecha_nacimiento"));
+            person.setEstadoCivil(rs.getString("estado_civil"));
             person.setCorreo(rs.getString("correo"));
             person.setContrasena(rs.getString("contrasena"));
 
         } catch (Exception e) {
-            e.getMessage();
+            System.out.println("DP_ERR_02");
         }finally {
             ConnectionDB.closeConnections(con,pstm);
         }
-
         return person;
     }
 
     // Create
     public boolean create(BeanPerson person){
         boolean flag = false;
-
         try{
             con = ConnectionDB.getConnection();
-            pstm = con.prepareStatement("INSERT INTO persona(nombre,aPaterno,aMaterno,edad,sexo,telefono,direccion,fechaNacimiento,estadoCivil,correo,contrasena) VALUES (?,?,?,?,?,?,?,?,?,?,?);");
+            pstm = con.prepareStatement("INSERT INTO persona(nombre, a_paterno, a_materno, edad, sexo, telefono, direccion, " +
+                    "fecha_nacimiento, estado_civil, correo, contrasena) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", PreparedStatement.RETURN_GENERATED_KEYS);
             pstm.setString(1,person.getNombre());
             pstm.setString(2,person.getaPaterno());
             pstm.setString(3,person.getaMaterno());
-            pstm.setInt(4,person.getEdad());
+            pstm.setLong(4,person.getEdad());
             pstm.setString(5,person.getSexo());
             pstm.setString(6,person.getTelefono());
             pstm.setString(7,person.getDireccion());
             pstm.setString(8,person.getFechaNacimiento());
-            pstm.setString(9,person.isEstadoCivil());
+            pstm.setString(9,person.getEstadoCivil());
             pstm.setString(10,person.getCorreo());
             pstm.setString(11,person.getContrasena());
 
-            flag = pstm.execute();
+            rs = pstm.getGeneratedKeys();
+            if(pstm.executeUpdate() == 1 && rs.first()){
+                pstm = con.prepareStatement("SELECT * FROM person WHERE id = ?");
+                pstm.setLong(1, rs.getLong(1));
 
+                if(rs.next()){
+                    //QUERY PARA GENERAR LA OPERACIÓN
+                    flag = true;
+                }
+            }
         }catch (SQLException e){
-            e.getMessage();
+            System.out.println("DP_ERR_03");
         }finally {
             ConnectionDB.closeConnections(con,pstm);
         }
@@ -116,8 +121,8 @@ public class DaoPerson {
         boolean flag = false;
         try{
             con = ConnectionDB.getConnection();
-            pstm = con.prepareStatement("UPDATE persona SET nombre=?,aPaterno=?,aMaterno=?,edad=?,sexo=?,telefono=?,direccion=?,fechaNacimiento=?,estadoCivil=?,correo=?,contrasena=? WHERE idPersona=?");
-
+            pstm = con.prepareStatement("UPDATE persona SET nombre = ?, a_paterno = ?, a_materno = ?, edad = ?, sexo = ?, telefono = ?, " +
+                    "direccion = ?, fecha_nacimiento = ?, estado_civil = ?, correo = ? WHERE idPersona = ?");
             pstm.setString(1,person.getNombre());
             pstm.setString(2,person.getaPaterno());
             pstm.setString(3,person.getaMaterno());
@@ -126,19 +131,21 @@ public class DaoPerson {
             pstm.setString(6,person.getTelefono());
             pstm.setString(7,person.getDireccion());
             pstm.setString(8,person.getFechaNacimiento());
-            pstm.setString(9,person.isEstadoCivil());
+            pstm.setString(9,person.getEstadoCivil());
             pstm.setString(10,person.getCorreo());
-            pstm.setString(11,person.getContrasena());
-            pstm.setInt(12,person.getIdPerson());
+            pstm.setLong(11, person.getId());
 
-            flag = pstm.execute();
+            if(pstm.executeUpdate() == 1){
+                // QUERY PARA REGISTRAR LA OPERACIÓN
+
+                flag = true;
+            }
         }catch (SQLException e){
-            e.getMessage();
+            System.out.println("DP_ERR_04");
         }finally {
             ConnectionDB.closeConnections(con,pstm);
         }
         return flag;
-
     }
 
 
@@ -156,6 +163,7 @@ public class DaoPerson {
             ConnectionDB.closeConnections(con,pstm);
         }
     }
+
     // Delete
     public boolean delete(int id){
         boolean flag=false;
